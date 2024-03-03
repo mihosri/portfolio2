ec7d2ae3f9b56ede31589e897fbab9e8d1c02d25
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-03 16:40:29 -0700

    Header component added
    Added header menus and initial styling for alignment

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between px-5">
+      <a href="#">Sri</a>
+      <ul className="flex">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </header>
+  )
+}
