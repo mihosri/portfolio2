ccf80a44da79fcb0c5d48ebf7b01843c57c10588
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-04 13:29:10 -0700

    Added mobile responsiveness to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,54 @@
+import { Bars3Icon } from '@heroicons/react/24/solid'
+import { useState } from 'react'
+
 export default function Header() {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <header className="flex justify-between px-5 py-2 bg-primary text-white">
       <a className="font-bold" href="#">
         Sri
       </a>
-      <ul className="flex">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
+      <nav className="hidden md:block">
+        <ul className="flex">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Mobile view menu */}
+
+      {toggleMenu && <nav className="block md:hidden mobile-nav">
+        <ul className="flex flex-col">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>}
+      
+      <button className="block md:hidden" onClick={()=> setToggleMenu(!toggleMenu)}>
+        <Bars3Icon className="text-white h-5" />
+      </button>
     </header>
   )
 }
