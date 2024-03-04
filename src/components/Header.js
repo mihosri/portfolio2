1dbbe77f2c264c46093778b60b56d35f35a68739
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-04 12:45:56 -0700

    styling for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
-    <header className="flex justify-between px-5">
-      <a href="#">Sri</a>
+    <header className="flex justify-between px-5 py-2 bg-primary text-white">
+      <a className="font-bold" href="#">
+        Sri
+      </a>
       <ul className="flex">
         <li>
           <a href="#">Home</a>
