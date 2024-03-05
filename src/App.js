44f9fe5a916f10426de42d2ae4bd7288c3b20fe7
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-05 15:15:57 -0700

    Sri Component Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header'
+import Sri from './components/Sri'
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Sri/>
     </div>
   )
 }
---
+++ src/components/Sri.js
@@ -0,0 +1,5 @@
+export default function Sri() {
+    return(
+        <section>Hi, I am Sri</section>
+    )
+}
