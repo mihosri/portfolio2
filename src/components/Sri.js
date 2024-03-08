8a7f70124ebfbf65c07725eba430de5e81f5fc98
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-07 23:25:41 -0700

    Update Sri.js

--- src/components/Sri.js
+++ src/components/Sri.js
@@ -1,5 +1,11 @@
 export default function Sri() {
-    return(
-        <section>Hi, I am Sri</section>
+    return (
+      <section>
+        <h1>
+          Hi,
+          <br /> I am Sri
+          <p>I am a Front end engineer</p>
+        </h1>
+      </section>
     )
 }
