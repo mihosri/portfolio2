b02ed60566f63c92402aa1c4ab487c48f9295ce5
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-08 23:26:43 -0700

    added image and styling for sri component

--- src/components/Sri.js
+++ src/components/Sri.js
@@ -1,11 +1,14 @@
+import sri from '../assets/sri.png'
+
 export default function Sri() {
-    return (
-      <section>
-        <h1>
-          Hi,
-          <br /> I am Sri
-          <p>I am a Front end engineer</p>
-        </h1>
-      </section>
-    )
+  return (
+    <section className="flex px-5 py-32 bg-secondary justify-center">
+      <h1 className="w-1/3 text-white text-4xl">
+        Hi,
+        <br /> I am Sri
+        <p>I am a Front end engineer</p>
+      </h1>
+      <img className="w-1/2" src={sri} />
+    </section>
+  )
 }
