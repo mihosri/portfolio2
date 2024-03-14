8c4c39ffd5556385d53187674e8dd848a24379e6
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-13 21:34:37 -0600

    Added social media icons

--- src/components/Sri.js
+++ src/components/Sri.js
@@ -1,14 +1,26 @@
 import sri from '../assets/sri.png'
+import { AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai'
 
 export default function Sri() {
   return (
     <section className="flex px-5 py-32 bg-secondary justify-center">
-      <h1 className="w-1/3 text-white text-4xl">
-        Hi,
-        <br /> I am Sri
-        <p>I am a Front end engineer</p>
-      </h1>
-      <img className="w-1/2" src={sri} />
+      <div className="w-1/2 flex flex-col">
+        <h1 className="text-white text-6xl font-sri-font">
+          Hi,
+          <br /> I am Sri
+          <p>I'm a Front end engineer</p>
+        </h1>
+        <div className="flex">
+          <a href="#">
+            <AiOutlineLinkedin size={40} />
+          </a>
+          <a href="#">
+            <AiOutlineGithub size={40} />
+          </a>
+          <a href="#"></a>
+        </div>
+      </div>
+      <img className="w-1/3" src={sri} />
     </section>
   )
 }
