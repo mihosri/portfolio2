e018383b1276036d2828707994148cf6e4e84d7e
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-13 21:55:12 -0600

    added mobile responsiveness to Sri component

--- src/components/Sri.js
+++ src/components/Sri.js
@@ -3,24 +3,30 @@ import { AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai'
 
 export default function Sri() {
   return (
-    <section className="flex px-5 py-32 bg-secondary justify-center">
-      <div className="w-1/2 flex flex-col">
+    <section className="flex flex-col md:flex-row px-5 py-32 bg-secondary justify-center">
+      <div className="md:w-1/2 flex flex-col">
         <h1 className="text-white text-6xl font-sri-font">
           Hi,
           <br /> I am Sri
-          <p>I'm a Front end engineer</p>
+          <p>I'm a Frontend engineer</p>
         </h1>
-        <div className="flex">
-          <a href="#">
+        <div className="flex py-10">
+          <a
+            href="https://www.linkedin.com/in/sri-guru-girahha/"
+            className="pr-5 hover:text-white"
+          >
             <AiOutlineLinkedin size={40} />
           </a>
-          <a href="#">
+          <a
+            href="https://github.com/mihosri"
+            className="pr-5 hover:text-white"
+          >
             <AiOutlineGithub size={40} />
           </a>
           <a href="#"></a>
         </div>
       </div>
-      <img className="w-1/3" src={sri} />
+      <img className="md:w-1/3" src={sri} />
     </section>
   )
 }
