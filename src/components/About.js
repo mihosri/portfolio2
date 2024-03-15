0f47692806824922dc73c46d6a1990b32203a4c7
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-14 22:59:59 -0600

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+export default function About(){
+    return <section>
+        <div>
+            <img src="" />
+        </div>
+        <div>
+            <h1>About Me</h1>
+            <p>Some information about myself</p>
+        </div>
+    </section>
+}
