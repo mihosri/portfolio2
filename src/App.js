32f2ad9c341654a38564e15cff2a7ad5ffdc3b2e
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-16 23:22:04 -0600

    About section image added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
+import About from './components/About'
 import Header from './components/Header'
 import Sri from './components/Sri'
 
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <Sri/>
+      <Header />
+      <Sri />
+      <About />
     </div>
   )
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,17 @@
-export default function About(){
-    return <section>
-        <div>
-            <img src="" />
-        </div>
-        <div>
-            <h1>About Me</h1>
-            <p>Hello I am a passionate Front End Engineer who is skilled in HTML, CSS, JavaScript, TypeScript and React.</p>
-        </div>
+import AboutImg from '../assets/aboutImg.jpg'
+export default function About() {
+  return (
+    <section className='flex'>
+      <div>
+        <img src={AboutImg} />
+      </div>
+      <div>
+        <h1>About Me</h1>
+        <p>
+          Hello I am a passionate Front End Engineer who is skilled in HTML,
+          CSS, JavaScript, TypeScript and React.
+        </p>
+      </div>
     </section>
+  )
 }
