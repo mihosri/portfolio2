387c73ed28f68dbb659c65a63a24a9ceb2286d47
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-18 23:55:59 -0600

    Update Sri.js

--- src/components/Sri.js
+++ src/components/Sri.js
@@ -3,7 +3,7 @@ import { AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai'
 
 export default function Sri() {
   return (
-    <section className="flex flex-col md:flex-row px-5 py-32 bg-secondary justify-center">
+    <section className="flex flex-col md:flex-row px-5 py-32 bg-primary justify-center">
       <div className="md:w-1/2 flex flex-col">
         <h1 className="text-white text-6xl font-sri-font">
           Hi,
