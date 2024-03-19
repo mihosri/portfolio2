7a9735a313c16ea022e8153f5937613c8825a2d8
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-18 23:54:24 -0600

    About Me styling

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,16 @@ export default function About() {
       <div className="w-1/2">
         <img src={AboutImg} />
       </div>
-      <div className="w-1/2">
-        <h1>About Me</h1>
-        <p>
-          Hello I am a passionate Front End Engineer who is skilled in HTML,
-          CSS, JavaScript, TypeScript and React.
-        </p>
+      <div className="w-1/2 flex justify-center">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-4xl text-white border-b-4 border-[#2b2d77] mb-5 w-[170px] font-bold">
+            About Me
+          </h1>
+          <p className="text-white">
+            Hello I am a passionate Front End Engineer who is skilled in HTML,
+            CSS, JavaScript, TypeScript and React.
+          </p>
+        </div>
       </div>
     </section>
   )
