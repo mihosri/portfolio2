792372155bc7f369a0aee17616a0fc246394f886
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-20 23:08:39 -0600

    changes in about

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
       </div>
       <div className="w-1/2 flex justify-center">
         <div className="flex flex-col justify-center">
-          <h1 className="text-4xl text-white border-b-4 border-[#2b2d77] mb-5 w-[170px] font-bold">
+          <h1 className="text-4xl text-white border-b-4 border-primary mb-5 w-[170px] font-bold">
             About Me
           </h1>
           <p className="text-white">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        primary: 'rgb(85, 81, 227)',
+      },
+    },
     fontFamily: {
       'sri-font': 'Sriracha',
     },
